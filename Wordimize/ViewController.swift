ea1f5223aed31fb7b2546bc6a4c52708d3885b3c
Stivan Mersho <stivan.ms@outlook.com>
2020-04-02 21:25:10 +0200

    Initial Commit

---
+++ Wordimize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wordimize
+//
+//  Created by Stivan Mersho on 2020-04-02.
+//  Copyright © 2020 Stivan Mersho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
